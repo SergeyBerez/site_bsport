42abb6599b8d78f4bdf258d8fb633e226456eb52
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2022-01-16 23:35:14 +0200

    add katalog

--- pages/components/Card.js
+++ pages/components/Card.js
@@ -8,7 +8,7 @@ import style from './cardsStyle.module.css';
 import Image from 'next/image';
 import { useEffect, useRef, useState } from 'react';
 import Detail from '../components/detail';
-export default function Card({ id, title, description, price, url }) {
+export default function Card({ id, title, description, price, url, titlePage, cssProps }) {
   const router = useRouter();
   const [image, setImage] = useState(
     'https://firebasestorage.googleapis.com/v0/b/b-sportwear-shop.appspot.com/o/no_image.png?alt=media&token=47b4ea63-cf4a-4b67-9fa7-8e8004f97505',
@@ -16,22 +16,31 @@ export default function Card({ id, title, description, price, url }) {
 
   const goToCardDetail = (e) => {
     e.stopPropagation();
-    console.log('ddd');
-    router.push(`${router.pathname}/${id}`);
+
+    if (cssProps) {
+      console.log(' if  goToCardDetail' + router.pathname);
+      router.push(`${cssProps}`);
+    } else {
+      console.log(' else  goToCardDetail' + router.pathname);
+      router.push(`${router.pathname}/${id}`);
+    }
   };
 
   useEffect(() => {
     if (url) {
       setImage(url);
+      console.log('useEffect   ' + cssProps + 'useEffect   ' + router.pathname);
     }
   }, [url]);
   return (
     <>
-      <div className={style.productCard_block} onClick={goToCardDetail}>
+      <div
+        onClick={goToCardDetail}
+        className={!cssProps ? style['productCard_block'] : style['productCard_block-katalog']}>
+        {titlePage ? <h5>{titlePage}</h5> : null}
         <Image src={image} width={250} height={300} alt="logo"></Image>
-        <h4 className={style['product-card__title']}>{title}</h4>
-
-        <div className={style['block_price']}>{price} грн</div>
+        {title ? <h4 className={style['product-card__title']}>{title}</h4> : null}
+        {price ? <div className={style['block_price']}>{price} грн</div> : null}
         {/* <div onClick={goToCardDetail} className={style.block_product}></div> */}
       </div>
     </>
--- pages/components/CardDetail.js
+++ pages/components/CardDetail.js
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 
@@ -19,37 +18,35 @@ export default function CardDetail({ id, title, description, price, url }) {
   return (
     <>
       <div className="productCard_block">
-        <div className="productCard_rightSide">
-          <p className="block_model">
-            <span className="block_model__text">Model: </span>
-            <span className="block_model__number">505795</span>
+        <p className="block_model">
+          <span className="block_model__text">Model: </span>
+          <span className="block_model__number">505795</span>
+        </p>
+
+        <div className="block_product">
+          <h2 className="block_name block_name__mainName">
+            <sup>&reg; </sup>
+          </h2>
+          <h2 className="block_name block_name__addName">{title}</h2>
+
+          <p className="block_product__advantagesProduct">
+            Wireless headphones with integrated microphone
           </p>
 
-          <div className="block_product">
-            <h2 className="block_name block_name__mainName">
-              <sup>&reg; </sup>
-            </h2>
-            <h2 className="block_name block_name__addName">{title}</h2>
+          <div className="block_descriptionInformation">
+            <span>{description}</span>
+          </div>
+          <Image src={image} width={300} height={300} alt="logo"></Image>
+          <div className="block_price">
+            <p className="block_price__currency">{price}</p>
+            <p className="block_price__shipping">Shipping and taxes extra</p>
+            <button className="button button_addToCard">add to card</button>
+          </div>
 
-            <p className="block_product__advantagesProduct">
-              Wireless headphones with integrated microphone
-            </p>
-
-            <div className="block_informationAboutDevice">
-              <div className="block_descriptionInformation">
-                <span>{description}</span>
-              </div>
-              <Image src={image} width={300} height={300} alt="logo"></Image>
-              <div className="block_price">
-                <p className="block_price__currency">{price}</p>
-                <p className="block_price__shipping">Shipping and taxes extra</p>
-                <button className="button button_addToCard">add to card</button>
-              </div>
-
-              <div className="block_goodColor">
-                <span className="text_specification">Choose your colors:</span>
-                <div className="block_goodColor__allColors">
-                  {/* <input
+          <div className="block_goodColor">
+            <span className="text_specification">Choose your colors:</span>
+            <div className="block_goodColor__allColors">
+              {/* <input
                     type="radio"
                     name="colorOfItem"
                     className="radio_button"
@@ -68,12 +65,21 @@ export default function CardDetail({ id, title, description, price, url }) {
                   <label
                     htmlFor="radioColor2"
                     className="block_goodColor__radio block_goodColor__silver"></label> */}
-                </div>
-              </div>
             </div>
           </div>
         </div>
       </div>
+      <style jsx>{`
+        .block_product {
+          flex: 1;
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          padding: 5px;
+          max-width: 350px;
+          justify-content: space-around;
+        }
+      `}</style>
     </>
   );
 }
