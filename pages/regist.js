08c27ea31bbc7b103b1cf947e6cd7efbf76cc982
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2022-01-27 10:28:02 +0200

    add input

--- pages/regist.js
+++ pages/regist.js
@@ -3,70 +3,201 @@ import MainLayout from '../components/MainLayout';
 export default function regist() {
   return (
     <MainLayout>
-      {/* <div
-        className="block-content"
-        aria-labelledby="block-customer-login-heading-btn"
-        data-role="content"
-        role="tabpanel"
-        data-collapsible="true"
-        aria-hidden="true"
-        style="display: none;">
-        <form
-          className="form form-login"
-          action="https://4fstore.ru/customer/account/loginPost/"
-          method="post"
-          id="login-form"
-          novalidate="novalidate">
-          <input name="form_key" type="hidden" value="fw1BR4yTTDz1DxRS"></input>{' '}
-          <fieldset className="fieldset login" data-hasrequired="* Обязательные поля">
-            <div className="field note">
-              If you have an account, sign in with your email address.
-            </div>
-            <div className="field email required">
-              <label className="label" for="email">
-                <span>E-mail:</span>
-              </label>{' '}
-              <div className="control">
-                <input
-                  name="login[username]"
-                  value=""
-                  autocomplete="off"
-                  id="email"
-                  type="email"
-                  className="input-text"
-                  title="Email"
-                  data-validate="{required:true, 'validate-email':true}"
-                  aria-required="true"></input>
+      <div className="block block-customer-login">
+        <div
+          className="block-title"
+          // role="tablist"
+        >
+          <strong
+            // className="hidden-mobile"
+            id="block-customer-login-heading"
+            // role="heading"
+            // aria-level="2"
+          >
+            Зарегистрированные клиенты
+          </strong>{' '}
+        </div>
+        <div
+          className="block-content"
+          // aria-labelledby="block-customer-login-heading-btn"
+          // data-role="content"
+          // role="tabpanel"
+          // data-collapsible="true"
+          // aria-hidden="true"
+          style={{ display: 'static' }}>
+          <form
+            className="form form-login"
+            id="login-form"
+            // novalidate="novalidate"
+          >
+            <input name="form_key" type="hidden" value="fw1BR4yTTDz1DxRS"></input>{' '}
+            <fieldset className="fieldset login" data-hasrequired="* Обязательные поля">
+              {/* <div className="field note">
+                If you have an account, sign in with your email address.
+              </div> */}
+              <div className="field email required">
+                <label className="label" htmlFor="email">
+                  <span>E-mail:</span>
+                </label>{' '}
+                <div className="control">
+                  <input
+                    name="login[username]"
+                    value=""
+                    autoComplete="off"
+                    id="email"
+                    type="email"
+                    className="input-text"
+                    title="Email"
+                    // data-validate="{required:true, 'validate-email':true}"
+                    // aria-required="true"
+                  ></input>
+                </div>
               </div>
-            </div>
-            <div className="field password required">
-              <label htmlfor="pass" className="label">
-                <span>Пароль:</span>
-              </label>{' '}
-              <div className="control">
-                <input
-                  name="login[password]"
-                  type="password"
-                  autocomplete="off"
-                  className="input-text"
-                  id="pass"
-                  title="Пароль"
-                  data-validate="{required:true}"
-                  aria-required="true"></input>
+              <div className="field password required">
+                <label htmlFor="pass" className="label">
+                  <span>Пароль:</span>
+                </label>{' '}
+                <div className="control">
+                  <input
+                    name="login[password]"
+                    type="password"
+                    autoComplete="off"
+                    className="input-text"
+                    id="pass"
+                    title="Пароль"
+                    // data-validate="{required:true}"
+                    // aria-required="true"
+                  ></input>
+                </div>
               </div>
-            </div>
-            <div className="actions-toolbar">
-              <div className="primary">
-                <button type="submit" className="action login primary" name="send" id="send2">
-                  <span>Войти</span>
-                </button>
+              <div className="actions-toolbar">
+                <div className="primary">
+                  <button
+                    type="submit"
+                    className="action login primary"
+                    name="send"
+                    // id="send2"
+                  >
+                    <span>Войти</span>
+                  </button>
+                </div>
               </div>
-            </div>
-          </fieldset>
-        </form>
-      </div> */}
+            </fieldset>
+          </form>
+        </div>
+      </div>
+      <style jsx>{`
+        .block.block-customer-login {
+          display: flex;
 
-      {/* <style jsx ></style> */}
+          flex-direction: column;
+        }
+        .block-content {
+          display: flex;
+
+          flex-direction: column;
+        }
+
+        .block-title {
+          color: #323232;
+
+          font-style: normal;
+          font-weight: 700;
+          line-height: 1;
+          font-size: 2rem;
+          text-transform: uppercase;
+          text-align: center;
+        }
+        .form {
+          display: block;
+          margin-top: 0em;
+        }
+        .form form-login {
+        }
+        .field {
+          margin: 0 0 25px;
+          line-height: 1;
+        }
+        .fieldset {
+          border: 0;
+          margin: 0 0 44px;
+          padding: 0;
+          letter-spacing: -0.31em;
+        }
+        .label {
+          margin: 0 0 14px;
+          color: #323232;
+          letter-spacing: 0.1rem;
+          font-style: normal;
+          font-weight: 300;
+          line-height: 1;
+          font-size: 1.5rem;
+        }
+        .label:after {
+          content: '*';
+          color: #000;
+          font-size: 1.4rem;
+          margin: 0 0 0 1px;
+        }
+        input[type='email'],
+        input[type='password'] {
+          background: #fff;
+          background-clip: padding-box;
+          border: 1px solid #cbcbcb;
+          border-radius: 0;
+          color: #323232;
+          font-family: LabGrotesque;
+          font-size: 14px;
+          font-weight: 300;
+          height: 32px;
+          line-height: 1.42857143;
+          padding: 0 11px;
+          vertical-align: baseline;
+          width: 100%;
+          box-sizing: border-box;
+        }
+
+        .control {
+          display: flex;
+
+          flex-direction: column;
+        }
+
+        .actions-toolbar {
+          padding-top: 3px;
+        }
+        // .login-container .actions-toolbar .secondary {
+        //   margin-bottom: 25px;
+        //   text-align: right;
+        //   width: 100%;
+        // }
+        // .login-container .actions-toolbar .secondary a.action {
+        //   margin-top: 0;
+        // }
+        // .login-container .actions-toolbar .primary {
+        //   width: 100%;
+        // }
+        .primary {
+          width: 100%;
+        }
+        .action.primary {
+          border: 2px solid #000;
+          width: 100%;
+          color: #fff;
+          background-color: #000;
+          font-size: 14px;
+          font-weight: 900;
+          padding: 16px 32px 14px;
+          line-height: 1;
+          text-transform: uppercase;
+          letter-spacing: 1.4px;
+        }
+        .action.primary:hover {
+          background: #444;
+          border: 2px solid #000;
+          color: #fff;
+        }
+      `}</style>
     </MainLayout>
   );
 }
