d8049923a364c4f442a57418a1b1a83bc4dad30f
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2022-04-24 16:17:27 +0300

    add Catalog

--- pages/catalog.js
+++ pages/catalog.js
@@ -4,7 +4,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
-export default function catalog() {
+export default function Catalog() {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     setLoading(false);
