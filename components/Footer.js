0d144f30b722a32f114babd30a66317061372fe4
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2022-07-11 20:06:20 +0300

    add description

--- components/Footer.js
+++ components/Footer.js
@@ -3,24 +3,65 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-box">1
+      <div className="footer-col">
+        <span className="footer-title">ДОПОМОГА ТА ІНФОРМАЦІЯ</span>
+        <ul>
+          <li>Правила магазину</li>
+          <li>Співпраця</li>
+          <li> Повернення і рекламація</li>
+          <li> Контакт</li>
+        </ul>
       </div>
-      <div className="footer-box">2</div>
-      <div className="footer-box">3</div>
+      <div className="footer-col">
+        {' '}
+        <span className="footer-title">Доставка</span>
+        <ul>
+          <li>У відділення Нова Пошта</li>
+          <li>укр пошта</li>
+        </ul>
+      </div>
+      <div className="footer-col">
+        {' '}
+        <span className="footer-title">Оплата</span>
+        <ul>
+          <li>У відділення Нова Пошта</li>
+          <li>укр пошта</li>
+        </ul>
+      </div>
+      <div className="footer-col">
+        {' '}
+        <span className="footer-title">Стежте за нами:</span>
+      </div>
       <style jsx>{`
         .footer {
           display: flex;
           height: 200px;
-          flex: 1;
+          flex: 0 1 auto;
           padding: 2rem 0;
           border-top: 1px solid #eaeaea;
-          justify-content: center;
-          align-items: center;
+         
           background: #fff;
         }
-        .footer-box {
+        .footer-col {
           display: flex;
           flex-direction: column;
+          flex: 1 1;
+          width: auto;
+          padding: 0 32px 32px
+          border-right: 1px solid #ededed;
+        }
+        .footer-col ul>li{
+         padding: 10px 0;
+         margin-left: 0;
+        }
+        .footer-title {
+          font-size: 1rem;
+          font-weight: 700;
+          display: block;
+          line-height: 1.375rem;
+          margin: 24px 0;
+          color: #000;
+          text-transform: uppercase;
         }
       `}</style>
     </footer>
