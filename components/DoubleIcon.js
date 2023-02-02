348d5083ddde8c644320185c4d90b57e96f56cdd
SergeyBerez <sergeyberezovsky1981@gmail.com>
2023-02-02 21:15:39 +0200

    add togle icon

--- components/DoubleIcon.js
+++ components/DoubleIcon.js
@@ -1,31 +1,34 @@
 import React from "react";
 import Image from "next/image";
 import square from "../public/static/img/351984_crop_square_icon.svg";
-import menu from "../public/static/img/9004778_menu_list_navigation_options_icon.svg";
-const Doubleicon = ({ toogleGood }) => {
+import menu from "../public/static/img/7787586_menu_navigation_direction_arrow_location_icon.svg";
+const Doubleicon = ({ toogleGood, show }) => {
+  console.log(show);
   return (
     <div className="toogle-icon">
-      {" "}
-      <div className="icon-show-one">
-        {" "}
-        <Image
-          onClick={toogleGood}
-          src={square}
-          width={30}
-          height={30}
-          alt="productOneGood"
-          name="name"
-        ></Image>
-      </div>
-      <div className="icon-show-two">
-        <Image
-          onClick={toogleGood}
-          src={menu}
-          width={20}
-          height={20}
-          alt="productTwoGood"
-        ></Image>
-      </div>
+      {!show ? (
+        <div className="icon-show-one">
+          {" "}
+          <Image
+            onClick={toogleGood}
+            src={square}
+            width={30}
+            height={30}
+            alt="productOneGood"
+            name="name"
+          ></Image>
+        </div>
+      ) : (
+        <div className="icon-show-two">
+          <Image
+            onClick={toogleGood}
+            src={menu}
+            width={30}
+            height={30}
+            alt="productTwoGood"
+          ></Image>
+        </div>
+      )}{" "}
     </div>
   );
 };
