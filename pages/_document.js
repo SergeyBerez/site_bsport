c6201fb1c778cb0a3edcbaa440648fae0eec7319
SergeyBerez <sergeyberezovsky1981@gmail.com>
2023-07-01 16:59:46 +0300

    add google analitic

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-PPG2W13CTS"
+          ></script>
+          <script>
+            if (typeof window !== "undefined")
+            {(window.dataLayer = window.dataLayer || [])(function gtag() {
+              dataLayer.push(arguments);
+            })(gtag("js", new Date()))(gtag("config", "G-PPG2W13CTS"))}
+          </script>
           <link
             href="https://cdn.ans-media.com/assets/front/ans/9.22.2-ans/static/media/euclid-circular-a-bold-web-xl.woff2"
             rel="stylesheet"
